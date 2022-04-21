e4a25705efae96ca5693e69172a8b2af59e6d19d
Umidjon Ibrokhimov <u.ibrokhimov1@gmail.com>
2022-04-21 17:18:19 +0500

    added agent create view

--- agents/views.py
+++ agents/views.py
@@ -1,14 +1,17 @@
+import random
 from django.views import generic
 from django.shortcuts import reverse
 from leads.models import Agent
-from django.contrib.auth.mixins import LoginRequiredMixin
+from .mixins import LoginRequiredMixin
+from django.core.mail import send_mail
 from .forms import AgentModelForm
 
 class AgentListView(LoginRequiredMixin,generic.ListView):
     template_name = "agents/agents_lists.html"
 
     def get_queryset(self):
-        return Agent.objects.all()
+        organisation = self.request.user.userprofile
+        return Agent.objects.filter(organisation=organisation)
 
 class AgentCreateView(LoginRequiredMixin, generic.CreateView):
     template_name = "agents/agents_create.html"
@@ -18,9 +21,23 @@ class AgentCreateView(LoginRequiredMixin, generic.CreateView):
         return reverse("agents:agent-list")
     
     def form_valid(self, form):
-        agent = form.save(commit=False)
-        agent.organisation = self.request.user.userprofile
-        agent.save()
+        user = form.save(commit=False)
+
+        # agent.organisation = self.request.user.userprofile
+        user.is_organiser = False
+        user.is_agent = True
+        user.set_password(f'{random.randint}')
+        user.save()
+        Agent.objects.create(
+            user=user,
+            organisation=self.request.user.userprofile
+        )
+        send_mail(
+            subject="Bu agent yaratilingan",
+            message="Yangi agent yarat",
+            from_email="test@test.com",
+            recipient_list=["test2@test.com"],
+        )
         return super(AgentCreateView, self).form_valid(form)
 
 class AgentDetailView(LoginRequiredMixin, generic.DetailView):
@@ -28,14 +45,16 @@ class AgentDetailView(LoginRequiredMixin, generic.DetailView):
     context_object_name = "agent"
 
     def get_queryset(self):
-        return Agent.objects.all()
+        organisation = self.request.user.userprofile
+        return Agent.objects.filter(organisation)
 
 class AgentUpdateView(LoginRequiredMixin, generic.UpdateView):
     template_name = "agents/agents_update.html"
     form_class = AgentModelForm
 
     def get_queryset(self):
-        return Agent.objects.all()
+        organisation = self.request.user.userprofile
+        return Agent.objects.filter(organisation)
 
     def get_success_url(self):
         return reverse("agents:agent-list")
@@ -45,7 +64,8 @@ class AgentDeleteView(LoginRequiredMixin, generic.DeleteView):
     context_object_name = "agent"
 
     def get_queryset(self):
-        return Agent.objects.all()
+        organisation = self.request.user.userprofile
+        return Agent.objects.filter(organisation)
 
     def get_success_url(self):
         return reverse("agents:agent-list")
--- leads/models.py
+++ leads/models.py
@@ -3,7 +3,8 @@ from django.db.models.signals import post_save
 from django.contrib.auth.models import AbstractUser
 
 class User(AbstractUser):
-    pass
+    is_organisor = models.BooleanField(default=True)
+    is_agent = models.BooleanField(default=False)
 
 class UserProfile(models.Model):
     user = models.OneToOneField(User, on_delete=models.CASCADE)
@@ -16,7 +17,8 @@ class Lead(models.Model):
     ismi = models.CharField(max_length=20)
     familiyasi = models.CharField(max_length=20)
     yoshi = models.IntegerField(default=0)
-    agent = models.ForeignKey("Agent", blank=True, on_delete=models.CASCADE)
+    organisation = models.ForeignKey(UserProfile, on_delete=models.CASCADE)
+    agent = models.ForeignKey("Agent", null=True, blank=True, on_delete=models.SET_NULL)
  
     def __str__(self):
         return str(self.familiyasi)
